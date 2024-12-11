e2779f04cc14d5eade5eabd915e00857cefec730
Jialing <davidluistoter@outlook.com>
2024-12-11 15:46:00 +0100

    completion of ahb_inf and ahb_sram_wrapper

--- rtl/ahb_inf.v
+++ rtl/ahb_inf.v
@@ -28,25 +28,91 @@ module ahb_inf #(
 	
 	// AHB slave outputs
 	output wire [DATA_WIDTH-1:0] hrdata_o,
-	output wire hreadyout_o,
+	output reg  hreadyout_o,
 	output wire hresp_o,
 	
 	// Memory interface
-	output wire mem_en_o,
+	output reg mem_en_o,
 	output wire mem_we_o,
-	output wire [3:0] mem_wbe_o,
-	output wire [ADDR_BITS-1:0] mem_addr_o,
+	output reg [3:0] mem_wbe_o,
+	output reg [ADDR_BITS-1:0] mem_addr_o,
 	output wire [DATA_WIDTH-1:0] mem_wdata_o,
 	input [DATA_WIDTH-1:0] mem_rdata_i
 
 );
+	
+	// mem_wdata can be directly fetched by ahb bus 
+	assign mem_wdata_o = hwdata_i;
+	
+	reg [ADDR_BITS-1:0] mem_addr_r;
+	
+	// haddr should be delayed for one beat and passed to mem_addr during writing
+	// if the last beat is write, first beat of reading should also delay haddr
+	// otherwise connect haddr_i to mem_addr
+	always @(posedge hclk_i or negedge hrst_n_i) begin
+		if (~hrst_n_i)	mem_addr_r <= 'b0;
+		else 						mem_addr_r <= haddr_i[2+:ADDR_BITS];
+	end
 
+	reg hwrite_r, hwrite_2r;
+	always @(posedge hclk_i or negedge hrst_n_i) begin
+		if (~hrst_n_i) begin
+			hwrite_r  <= 1'b0;
+			hwrite_2r <= 1'b0;
+		end else begin 						
+			hwrite_r  <= hwrite_i;
+			hwrite_2r <= hwrite_r;
+		end	
+	end
+	
+	always @(*) begin
+		if (hwrite_r)	mem_addr_o = mem_addr_r;			 // hwrite_r = 1 represents the addr phase is write cmd
+		else if (hwrite_2r)	mem_addr_o = mem_addr_r; // hwrite_r = 0, hwrite_2r = 1 represents the current transfer addr phase is read
+																							   // but the last beat transfer is write --> write switch to read
+		else mem_addr_o = haddr_i[2+:ADDR_BITS];									 // otherwise directly pass haddr_i
+	end
+	
+	// hstrb_i should be delayed for 1 cycle
+	// and pass to mem_wbe_o
+	always @(posedge hclk_i or negedge hrst_n_i) begin
+		if (~hrst_n_i)	mem_wbe_o <= 'b0;
+		else 						mem_wbe_o <= hstrb_i;
+	end	
 
+	// mem_en_o 
+	// for writing mem_en_o is simply hsel_r
+	// for reading mem_en_o is hsel_i
+	// from writing switch to reading, mem_en_o should be 1
+	// from reading switch to writing, mem_en_o should be 0 for 1 cycle
+	// for r -> w case: hwrite_i = 1 indicates in this cycle you initiate one beat write transfer
+	// meanwhile hwrite_r = 0 indicates the last beat transfer is read
+	reg hsel_r;
+	always @(posedge hclk_i or negedge hrst_n_i) begin
+		if (~hrst_n_i)	hsel_r <= 'b0;
+		else 						hsel_r <= hsel_i;
+	end		
+	
+	always @(*) begin
+		if (hwrite_r)	 			mem_en_o = hsel_r;	// consequtive write
+		else if (hwrite_2r)	mem_en_o = 1'b1;		// write switch to read
+		else if (hwrite_i)	mem_en_o = 1'b0;    // read switch to write
+		else 								mem_en_o = hsel_i;	// consequtive read
+	end
 
-
-
-
-
-
-
+	// mem_we_o
+	assign mem_we_o = hwrite_r;
+	
+	///////////////////////
+	// AHB slave output ///
+	///////////////////////
+	assign hrdata_o = mem_rdata_i;
+	assign hresp_o = 1'b0;
+	
+	// hreadyout_o, except for switch from write to read = 0, otherwise = 1
+	always @(*) begin
+		if ((~hwrite_r)& hwrite_2r)	hreadyout_o = 1'b0;
+		else hreadyout_o = 1'b1;
+	end
+	
+	
 endmodule : ahb_inf
--- rtl/ahb_sram_wrapper.v
+++ rtl/ahb_sram_wrapper.v
@@ -32,8 +32,50 @@ module ahb_sram_wrapper #(
 
 );
 
+	// connection signals
+	wire mem_en_s, mem_we_s;
+	wire [3:0] mem_wbe_s;
+	wire [ADDR_BITS-1:0] mem_addr_s;
+	wire [DATA_WIDTH-1:0] mem_wdata_s, mem_rdata_s;
 	
+	ahb_inf #(.MEM_DEPTH(MEM_DEPTH), .DATA_WIDTH(DATA_WIDTH), .ADDR_BITS(ADDR_BITS))
+		u_inf (
+			.hclk_i			        (hclk_i),
+			.hrst_n_i		        (hrst_n_i),
+			.hburst_i						(hburst_i),
+			.hmasterlock_i			(hburst_i),
+			.hprot_i						(hprot_i),
+			.hsize_i						(hsize_i),
+			.htrans_i						(htrans_i),
+			.hwrite_i						(hwrite_i),
+			.hstrb_i						(hstrb_i),
+			.haddr_i						(haddr_i),
+			.hwdata_i						(hwdata_i),
+			.hsel_i							(hsel_i),
+			.hready_i						(hready_i),
+			
+			.hrdata_o						(hrdata_o),
+			.hreadyout_o				(hreadyout_o),
+			.hresp_o						(hresp_o),
+			
+			.mem_en_o						(mem_en_s),
+			.mem_we_o						(mem_we_s),
+			.mem_wbe_o					(mem_wbe_s),
+			.mem_addr_o					(mem_addr_s),
+			.mem_wdata_o				(mem_wdata_s),
+			
+			.mem_rdata_i				(mem_rdata_s)
+		);
 
-
-
+	sp_sram_wbe4 #(.MEM_DEPTH(MEM_DEPTH), .DATA_WIDTH(DATA_WIDTH), .ADDR_BITS(ADDR_BITS))
+		u_sram (
+		  .clk_i							(hclk_i),
+		  .rst_n_i						(hrst_n_i),
+		  .en_i								(mem_en_s),
+		  .we_i								(mem_we_s),
+		  .wbe_i							(mem_wbe_s),
+		  .addr_i 						(mem_addr_s),
+      .wdata_i						(mem_wdata_s),
+		  .rdata_o						(mem_rdata_s)
+		);
 endmodule
